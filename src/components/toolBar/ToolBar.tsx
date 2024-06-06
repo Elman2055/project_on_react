9eb5039f7ab7ed4f0188ede82fc3e7fe4f8ff549
Elman <200522elmanok>
2024-06-06 20:25:45 +0500

    Реализовал SideBar

--- src/components/toolBar/ToolBar.tsx
+++ src/components/toolBar/ToolBar.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Logo from "../UI/images/Logo.png";
+import MyPicture from "../UI/images/MyPicture.png";
+import UpUnion from "../UI/images/UpUnion.png";
+import Profile from "../UI/images/Profile.png";
+import CheckList from "../UI/images/CheckList.png";
+import Message from "../UI/images/Message.png";
+import Gallerry from "../UI/images/Gallerry.png";
+import Group from "../UI/images/Group.png";
+import List from "../UI/images/List.png";
+import Money from "../UI/images/Money.png";
+import Cash from "../UI/images/Cash.png";
+import Cancel from "../UI/images/Cancel.png";
 import "./ToolBar.css";
 
 const ToolBar = () => {
   return (
     <div className="toolBar">
-
+      <div className="toolBarContainer">
+        <img src={Logo} alt="logo" />
+        <img src={MyPicture} alt="my_picture" className="myPicture" />
+        <img src={UpUnion} alt="up_union" />
+        <img src={Profile} alt="profile" />
+        <img src={CheckList} alt="check_list" />
+        <img src={Message} alt="message" />
+        <img src={Gallerry} alt="gallerry" />
+        <img src={Group} alt="group" />
+        <img src={List} alt="list" />
+        <img src={Money} alt="money" />
+        <img src={Cash} alt="cash" />   
+        <img src={Cancel} alt="cancel" />
+      </div>
     </div>
   );
 };
